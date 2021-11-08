f3b7dfa4f4c3305d7032abd25f41eb8f74404d4a
marcof90 <ingmarcosanabria@gmail.com>
2021-11-08 17:50:02 -0500

    environment variables, login, router

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { App } from './App'
-
-ReactDOM.render( <App /> , document.getElementById('root'))
+import { BrowserRouter } from 'react-router-dom'
+// import { App } from './App'
+import Router from './Router'
+require('dotenv').config()
+// ReactDOM.render( <App /> , document.getElementById('root'))
+ReactDOM.render( 
+    <BrowserRouter>
+        <Router />
+    </BrowserRouter>
+    , document.getElementById('root'))
